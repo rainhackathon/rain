eefcedd00d0c7cb06310f3e85cc1b9259c61c6ad
M Brynard <mbrynard@gmail.com>
2016-08-27 15:17:04 -0700

    Add authentication.

---
+++ www/js/controllers/accounts.js
@@ -0,0 +1,104 @@
+angular.module('starter.controllers.accounts', [])
+
+    .controller('LoginCtrl', function ($scope, $ionicModal, $state, $ionicLoading, $rootScope, User, $ionicPopup) {
+        'use strict';
+
+        $ionicModal.fromTemplateUrl('templates/accounts/signup.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modalSignup = modal;
+        });
+
+
+        $ionicModal.fromTemplateUrl('templates/accounts/forgot.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.ModalForgot = modal;
+        });
+
+        $scope.ShowModalSignup = function (){
+             $scope.modalSignup.show()
+        }
+
+        $scope.ShowModalForgot = function (){
+             $scope.ModalForgot.show()
+        }
+
+        $scope.CloseModalSignup = function (){
+             $scope.modalSignup.hide()
+        }
+
+        $scope.CloseModalForgot = function (){
+             $scope.ModalForgot.hide()
+        }
+
+        $scope.registerUser = function (form) {
+            if (form.$valid) {
+                $ionicLoading.show({
+                    template: 'Signing Up...'
+                });
+
+                User.register(form.first_name.$viewValue, form.email.$viewValue, form.company_id.$viewValue, form.password1.$viewValue, form.password2.$viewValue)
+                    .then(function (res) {
+                    if (res.status === 201) {
+                        $ionicLoading.hide();
+                        $state.go('app.home');
+                    } else {
+                        $ionicPopup.alert({title: "Error", template: res.data.message});
+                    }
+
+                    $ionicLoading.hide();
+                    $scope.CloseModalSignup();
+                }).catch(function (error) {
+                    $ionicPopup.alert({title: "Error", template: error});
+                    $ionicLoading.hide();
+                });
+            }
+        };
+
+        $scope.logIn = function (form) {
+            if (form.$valid) {
+                $ionicLoading.show({
+                    template: 'Logging In...'
+                });
+
+                User.login(form.email.$viewValue, form.company_id.$viewValue, form.password.$viewValue).then(function (res) {
+                    $ionicLoading.hide();
+
+                    if (res.status === 200) {
+                        $state.go('app.home');
+                    } else {
+                        $ionicPopup.alert({title: "Error", template: res.data.message});
+                    }
+                }).catch(function (error) {
+                    $ionicPopup.alert({title: 'Authentication failed', template: error.data.message});
+                    $ionicLoading.hide();
+                });
+            }
+        };
+
+        $scope.resetPassword = function (form) {
+            if (form.$valid) {
+                $ionicLoading.show({
+                    template: 'Sending email..'
+                });
+
+                $scope.CloseModalForgot();
+
+                User.resetPassword(form.email.$viewValue, form.company_id.$viewValue).then(function (res) {
+                    $ionicLoading.hide();
+
+                    if (res.status === 200) {
+                        $ionicPopup.alert({title: "Sent!", template: "Please check your inbox and follow the directions received in the email."});
+                    } else {
+                        $ionicPopup.alert({title: "Error", template: res.data.message});
+                    }
+                }).catch(function (error) {
+                    $ionicPopup.alert({title: 'Password Reset Error', template: error.data.message});
+                    $ionicLoading.hide();
+                });
+            }
+        };
+    });
---
+++ www/js/services/accounts.js
@@ -0,0 +1,138 @@
+/*global Firebase, console, angular */
+angular.module('starter.services.accounts', [])
+
+    .factory('authInterceptor', function (API, Auth, $location) {
+        'use strict';
+        return {
+            // automatically attach Authorization header
+            request: function (config) {
+                var token = Auth.getToken();
+
+                if (token && config.url.indexOf(API) === 0 && token) {
+                    config.headers.Authorization = 'JWT ' + token;
+                }
+
+                return config;
+            },
+
+            // If a token was sent back, save it
+            response: function (res) {
+                if (res.data) {
+                    if (res.config.url.indexOf(API) === 0 && res.data.token) {
+                        Auth.saveToken(res.data.token);
+                        Auth.saveUser(res.data.user);
+                    }
+                }
+
+                return res;
+            },
+            //Redirect to login if unauthorised
+            responseError: function (res) {
+                if (res.status === 401 || res.status === 403) {
+                    Auth.logout();
+                    $location.path('/login');
+                }
+
+                return res;
+            }
+        };
+    })
+
+    .service('Auth', function ($window) {
+        'use strict';
+
+        var self = this;
+
+        // Add JWT methods here
+        self.parseJwt = function (token) {
+            var base64Url = token.split('.')[1];
+            var base64 = base64Url.replace('-', '+').replace('_', '/');
+            return JSON.parse($window.atob(base64));
+        };
+
+        self.saveToken = function (token) {
+            $window.localStorage.setItem('jwtToken', token);
+        };
+
+        self.saveUser = function (user) {
+            $window.localStorage.setItem('user', JSON.stringify(user));
+        };
+
+        self.getToken = function () {
+            return $window.localStorage.getItem('jwtToken');
+        };
+
+        self.getUser = function () {
+            return JSON.parse($window.localStorage.getItem('user'));
+        };
+
+        self.isAuthed = function () {
+            var token = self.getToken();
+            if (token) {
+                var params = self.parseJwt(token);
+                return Math.round(new Date().getTime() / 1000) <= params.exp;
+            } else {
+                return false;
+            }
+        };
+
+        self.logout = function () {
+            $window.localStorage.removeItem('jwtToken');
+            $window.localStorage.removeItem('user');
+            $window.localStorage.removeItem('myTransactions');
+            $window.localStorage.removeItem('wiCode');
+        };
+    })
+
+    .service('User', function ($http, API, Auth) {
+        'use strict';
+        var self = this;
+
+        // add authentication methods here
+        self.register = function (first_name, email, company_id, password1, password2) {
+            return $http.post(API + '/accounts/register/', {
+                first_name: first_name,
+                email: email,
+                company_id: company_id,
+                password1: password1,
+                password2: password2
+            }).then(function (res) {
+                Auth.saveToken(res.data.token);
+                Auth.saveUser(res.data.user);
+                return res;
+            });
+        };
+
+        self.login = function (email, company_id, password) {
+            return $http.post(API + '/accounts/login/', {
+                email: email,
+                company_id: company_id,
+                password: password
+            })
+        };
+
+        self.resetPassword = function (email, company_id) {
+            return $http.post(API + '/accounts/password/reset/', {
+                email: email,
+                company_id: company_id
+            })
+        };
+
+        self.partialUpdate = function (jsonUpdate) {
+            return $http.patch(API + '/user/', jsonUpdate)
+        };
+
+        self.sendOtp = function () {
+            return $http.post(API + '/send_otp/', {});
+        };
+
+        self.verifyMobile = function (otp) {
+            return $http.post(API + '/verify_mobile/', {
+                'otp': otp
+            });
+        };
+
+        self.getCurrent = function () {
+            return Auth.getUser();
+        };
+    });
