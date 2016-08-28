aad37bcc0ead2902f234977431e21115fd06922d
M Brynard <mbrynard@gmail.com>
2016-08-28 02:40:05 -0700

    Add cash

--- www/js/app.js
+++ www/js/app.js
@@ -5,173 +5,198 @@
 //the 2nd parameter is an array of 'requires'
 //'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic',
-                           'ngMessages',
-                           'starter.controllers',
-                           'starter.controllers.accounts',
-                           'starter.controllers.transactions',
-                           'starter.controllers.menu',
-                           'starter.controllers.tips',
-                           'starter.controllers.deposits',
-                           'starter.services.accounts',
-                           'starter.services.transactions'])
+    'ngMessages',
+    'ionic.contrib.ui.tinderCards',
+    'starter.controllers',
+    'starter.controllers.accounts',
+    'starter.controllers.transactions',
+    'starter.controllers.menu',
+    'starter.controllers.tips',
+    'starter.controllers.deposits',
+    'starter.services.accounts',
+    'starter.services.transactions'])
 
 
-                           .constant('API', 'https://rehive.com/api/1')
-                           .constant('REFRESH_INTERVAL', 3000)
+  .constant('API', 'https://rehive.com/api/1')
+  .constant('REFRESH_INTERVAL', 3000)
 
-                           .config(function ($httpProvider, $ionicConfigProvider, $compileProvider) {
-                        	   'use strict';
-                        	   //Switch off caching:
-                        	   $ionicConfigProvider.views.maxCache(0);
-                        	   //Force buttons to bottom of screen:
-                        	   $ionicConfigProvider.tabs.position('bottom');
-                        	   //Insert JWT token into all api requests:
-                        	   $httpProvider.interceptors.push('authInterceptor');
-                           })
+  .directive('noScroll', function ($document) {
 
-                           .run(function ($ionicPlatform) {
-                        	   $ionicPlatform.ready(function () {
-                        		   // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                        		   // for form inputs)
-                        		   if (window.cordova && window.cordova.plugins.Keyboard) {
-                        			   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                        			   cordova.plugins.Keyboard.disableScroll(true);
+    return {
+      restrict: 'A',
+      link: function ($scope, $element, $attr) {
 
-                        		   }
-                        		   if (window.StatusBar) {
-                        			   // org.apache.cordova.statusbar required
-                        			   StatusBar.styleDefault();
-                        		   }
-                        	   });
-                           })
+        $document.on('touchmove', function (e) {
+          e.preventDefault();
+        });
+      }
+    }
+  })
 
-                           .config(function ($stateProvider, $urlRouterProvider) {
-                        	   $stateProvider
+  .config(function ($httpProvider, $ionicConfigProvider, $compileProvider) {
+    'use strict';
+    //Switch off caching:
+    $ionicConfigProvider.views.maxCache(0);
+    //Force buttons to bottom of screen:
+    $ionicConfigProvider.tabs.position('bottom');
+    //Insert JWT token into all api requests:
+    $httpProvider.interceptors.push('authInterceptor');
+  })
 
-                        	   // Accounts
-                        	   .state('login', {
-                        		   url: '/login',
-                        		   templateUrl: 'templates/accounts/login.html',
-                        		   controller: 'LoginCtrl'
-                        	   })
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-                        	   // Menu
-                        	   .state('app', {
-                        		   url: '/app',
-                        		   abstract: true,
-                        		   templateUrl: 'templates/elements/menu.html',
-                        		   controller: 'MenuCtrl'
-                        	   })
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-                        	   // Add Credit Card
-                        	   .state('app.add_credit_card', {
-                        		   url: '/add_credit_card',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/deposit/add_credit_card.html',
-                        				   controller: 'AddCreditCardCtrl'
-                        			   }
-                        		   }
-                        	   })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-                        	   // Choose Credit Card
-                        	   .state('app.choose_credit_card', {
-                        		   url: '/choose_credit_card',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/deposit/choose_credit_card.html',
-                        				   controller: 'ChooseCreditCardCtrl'
-                        			   }
-                        		   }
-                        	   })
+    // Accounts
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/accounts/login.html',
+        controller: 'LoginCtrl'
+      })
 
-                        	   // Create deposit
-                        	   .state('app.create_deposit', {
-                        		   url: '/create_deposit',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/deposit/create_deposit.html',
-                        				   controller: 'CreateDepositCtrl'
-                        			   }
-                        		   },
-                        		   params: {
-                        			   amount: null
-                        		   }
-                        	   })
+      // Menu
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/elements/menu.html',
+        controller: 'MenuCtrl'
+      })
 
-                        	   // Confirm deposit
-                        	   .state('app.confirm_deposit', {
-                        		   url: '/confirm_deposit',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/deposit/confirm_deposit.html',
-                        				   controller: 'ConfirmDepositCtrl'
-                        			   }
-                        		   },
-                        		   params: {
-                        			   amount: null
-                        		   }
-                        	   })
+      // Add Credit Card
+      .state('app.add_credit_card', {
+        url: '/add_credit_card',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/deposit/add_credit_card.html',
+            controller: 'AddCreditCardCtrl'
+          }
+        }
+      })
 
-                        	   // Success deposit
-                        	   .state('app.success_deposit', {
-                        		   url: '/success_deposit',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/deposit/success_deposit.html',
-                        				   controller: 'SuccessDepositCtrl'
-                        			   }
-                        		   },
-                        		   params: {
-                        			   amount: null
-                        		   }
-                        	   })
+      // Choose Credit Card
+      .state('app.choose_credit_card', {
+        url: '/choose_credit_card',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/deposit/choose_credit_card.html',
+            controller: 'ChooseCreditCardCtrl'
+          }
+        }
+      })
 
-                        	   // Scan Tip
-                        	   .state('app.scan_tip', {
-                        		   url: '/scan_tip',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/tip/scan_tip.html',
-                        				   controller: 'ScanTipCtrl'
-                        			   }
-                        		   }
-                        	   })
+      // Create deposit
+      .state('app.create_deposit', {
+        url: '/create_deposit',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/deposit/create_deposit.html',
+            controller: 'CreateDepositCtrl'
+          }
+        },
+        params: {
+          amount: null
+        }
+      })
 
-                        	   // Create Tip
-                        	   .state('app.create_tip', {
-                        		   url: '/create_tip',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/tip/create_tip.html',
-                        				   controller: 'CreateTipCtrl'
-                        			   }
-                        		   }
-                        	   })
+      // Confirm deposit
+      .state('app.confirm_deposit', {
+        url: '/confirm_deposit',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/deposit/confirm_deposit.html',
+            controller: 'ConfirmDepositCtrl'
+          }
+        },
+        params: {
+          amount: null
+        }
+      })
 
-                        	   // Confirm Tip
-                        	   .state('app.confirm_tip', {
-                        		   url: '/confirm_tip',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/tip/confirm_tip.html',
-                        				   controller: 'ConfirmTipCtrl'
-                        			   }
-                        		   }
-                        	   })
+      // Success deposit
+      .state('app.success_deposit', {
+        url: '/success_deposit',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/deposit/success_deposit.html',
+            controller: 'SuccessDepositCtrl'
+          }
+        },
+        params: {
+          amount: null
+        }
+      })
+
+      // Scan Tip
+      .state('app.scan_tip', {
+        url: '/scan_tip',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/tip/scan_tip.html',
+            controller: 'CardsCtrl'
+          }
+        }
+      })
+
+      // Flip Tip
+      .state('app.flip_tip', {
+        url: '/flip_tip',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/tip/flip_tip.html',
+            controller: 'CardsCtrl'
+          }
+        }
+      })
+
+      // Create Tip
+      .state('app.create_tip', {
+        url: '/create_tip',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/tip/create_tip.html',
+            controller: 'CreateTipCtrl'
+          }
+        }
+      })
+
+      // Confirm Tip
+      .state('app.confirm_tip', {
+        url: '/confirm_tip',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/tip/confirm_tip.html',
+            controller: 'ConfirmTipCtrl'
+          }
+        }
+      })
 
 
-                        	   .state('app.receive', {
-                        		   url: '/receive',
-                        		   views: {
-                        			   'menuContent': {
-                        				   templateUrl: 'templates/MyQRCode.html',
-                        				   controller: 'MyQRCodeCtrl'
-                        			   }
-                        		   }
-                        	   });
+      .state('app.receive', {
+        url: '/receive',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/MyQRCode.html',
+            controller: 'MyQRCodeCtrl'
+          }
+        }
+      });
 
-                        	   // if none of the above states are matched, use this as the fallback
-                        	   $urlRouterProvider.otherwise('/app/scan_tip');
-                           });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/scan_tip');
+  });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,15 +7,48 @@ angular.module('starter.controllers', [])
 .controller('MyQRCodeCtrl', function($scope, User) {
 	User.getInfo().then(function(res) {
 		$scope.email = res.data.data.email
-		
-		var qrUrl = 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' 
-			+ $scope.email 
+
+		var qrUrl = 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl='
+			+ $scope.email
 			+ '&choe=UTF-8';
-		
+
 		$scope.qrUrl = qrUrl;
 	})
 })
 
-;
+.controller('CardsCtrl', function($scope, TDCardDelegate) {
+  console.log('CARDS CTRL');
+  var cardTypes = [
+    { image: 'img/dollar.jpg' },
+    { image: 'img/dollar.jpg' },
+    { image: 'img/dollar.jpg' },
+    { image: 'img/dollar.jpg' }
+  ];
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0);
+
+    $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+    console.log('Hello');
+    $scope.addCard()
+  };
+})
+
+  .controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+});
 
 
