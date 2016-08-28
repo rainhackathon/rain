ed496bcb334aaf39ee6b64036c453dfb4b30efc8
Helghardt Avenant <helghardt@gmail.com>
2016-08-28 02:24:58 -0700

    Add QR code scanner.

--- www/js/controllers/tips.js
+++ www/js/controllers/tips.js
@@ -3,17 +3,38 @@ angular.module('starter.controllers.tips', [])
     .controller('ScanTipCtrl', function ($scope) {
         'use strict';
         $scope.data = {};
-        $scope.actions = "Scan QR";
+
+        $scope.scanQr = function () {
+            cordova.plugins.barcodeScanner.scan(
+                function (result) {
+
+                    console.log(result);
+
+                    $state.go('app.create_tip', {
+                        email: result.text
+                    });
+
+                    //alert("We got a barcode\n" +
+                    //    "Result: " + result.text + "\n" +
+                    //    "Format: " + result.format + "\n" +
+                    //    "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+            );
+        }
     })
 
     .controller('CreateTipCtrl', function ($scope) {
         'use strict';
         $scope.data = {};
-        $scope.actions = "Create Tip";
+
+
     })
 
     .controller('ConfirmTipCtrl', function ($scope) {
         'use strict';
-        $scope.data = {};
-        $scope.actions = "Confirm Tip";
+        $scope.email = $stateParams.email;
+
     });
